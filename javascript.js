85eb7b365b4419c3c0a7fe6fc60a23b63773c5f0
James Besherse <James.Besherse25@Gmail.com>
2019-08-06 13:42:26 -0700

    JS change/ Made it so you can close stat panels one at a time and not all at once

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
-function closeStatPanel() {
-  console.log(StatPanel);
-  var StatPanel = Array.from(document.getElementsByClassName("StatPanel"));
-  StatPanel.map(element => element.style.display = "none");
-}
-
 function openStatPanel(id) {
   console.log(StatPanel);
   var StatPanel = document.getElementById(id);
   StatPanel.style.display = "block";
+}
+
+function closeStatPanel(id) {
+  console.log(StatPanel);
+  var StatPanel = document.getElementById(id);
+  StatPanel.style.display = "none";
 }
